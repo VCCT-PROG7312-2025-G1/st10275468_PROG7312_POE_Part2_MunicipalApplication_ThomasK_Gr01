b15bddbbefcbb1f55373aa750603b1c3af125f69
st10275468 <st10275468@vcconnect.edu.za>
2025-10-13 12:32:45 +0200

    First commit of part 2: Created Event Model with attributes

---
+++ Municipal Services Portal/Models/Event.cs
@@ -0,0 +1,12 @@
+﻿namespace Municipal_Services_Portal.Models
+{
+    public class Event
+    {
+        public string eventName { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public DateTime Date { get; set; }
+       
+    }
+}
