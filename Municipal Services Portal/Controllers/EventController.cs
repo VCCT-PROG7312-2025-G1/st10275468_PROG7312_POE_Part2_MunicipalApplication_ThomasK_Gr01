fecb31a3bc4668734b6a07be489c6976fdd707d3
st10275468 <st10275468@vcconnect.edu.za>
2025-10-13 17:19:29 +0200

    Fixed sorting and filtering

--- Municipal Services Portal/Controllers/EventController.cs
+++ Municipal Services Portal/Controllers/EventController.cs
@@ -1,23 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Formatters;
 using Municipal_Services_Portal.Models;
+using System.Linq;
+
 namespace Municipal_Services_Portal.Controllers
 {
     public class EventController : Controller
     {
-        //Sorted Dictionary used to store the events sorted by date
+        // Sorted Dictionary used to store events by date
         private static SortedDictionary<DateTime, List<Event>> eventsSortedDate = new SortedDictionary<DateTime, List<Event>>();
 
-        //Hashset used to store the categories of the events
+        // HashSet used to store unique categories
         private static HashSet<string> categories = new HashSet<string>();
 
-        //Stack used to store the recently viewed events
+        // Stack used for recently viewed events
         private static Stack<Event> recentlyViewedEvents = new Stack<Event>();
-
 
         static EventController()
         {
-            //Sample events
+            // Sample events
             AddEvent("Event 1", "Category 1", new DateTime(2025, 10, 5), "Event 1 description", "Event 1 location", "/EventImages/marathon.jpg");
             AddEvent("Event 2", "Category 2", new DateTime(2025, 10, 12), "Event 2 description", "Event 2 location", "");
             AddEvent("Event 3", "Category 3", new DateTime(2025, 10, 18), "Event 3 description", "Event 3 location", "");
@@ -33,44 +33,49 @@ namespace Municipal_Services_Portal.Controllers
             AddEvent("Event 13", "Category 1", new DateTime(2025, 11, 29), "Event 13 description", "Event 13 location", "");
             AddEvent("Event 14", "Category 2", new DateTime(2025, 12, 3), "Event 14 description", "Event 14 location", "");
             AddEvent("Event 15", "Category 3", new DateTime(2025, 12, 7), "Event 15 description", "Event 15 location", "");
-
         }
-        /// <summary>
-        /// Method that adds an event to the sorted dictionary and the event categories to the category hashset
-        /// </summary>
-        /// <param name="name"></param>
-        /// <param name="category"></param>
-        /// <param name="date"></param>
-        /// <param name="description"></param>
-        /// <param name="location"></param>
-        /// <param name="path"></param>
+
         private static void AddEvent(string name, string category, DateTime date, string description, string location, string imgPath)
         {
-           
             Event newEvent = new Event(name, category, description, location, date, imgPath);
 
             if (!eventsSortedDate.ContainsKey(date))
-            {
                 eventsSortedDate[date] = new List<Event>();
-            }
+
             eventsSortedDate[date].Add(newEvent);
-
             categories.Add(category);
-        
-            
         }
-
 
         [HttpGet]
-        public IActionResult Events()
+        public IActionResult Events(string search = "", string categoryFilter = "", string sortOrder = "")
         {
-            var eventsList = eventsSortedDate.SelectMany(e => e.Value).OrderBy(e => e.Date).ToList();
+            var eventsList = eventsSortedDate.SelectMany(e => e.Value).ToList();
+
+            if (!string.IsNullOrEmpty(categoryFilter))
+                eventsList = eventsList.Where(e => e.Category == categoryFilter).ToList();
+
+            if (!string.IsNullOrEmpty(search))
+                eventsList = eventsList.Where(e => e.eventName.ToLower().Contains(search.ToLower())).ToList();
+
+            eventsList = sortOrder switch
+            {
+                "dateAsc" => eventsList.OrderBy(e => e.Date).ToList(),
+                "dateDesc" => eventsList.OrderByDescending(e => e.Date).ToList(),
+                "nameAsc" => eventsList.OrderBy(e => e.eventName).ToList(),
+                "nameDesc" => eventsList.OrderByDescending(e => e.eventName).ToList(),
+                "categoryAsc" => eventsList.OrderBy(e => e.Category).ToList(),
+                "categoryDesc" => eventsList.OrderByDescending(e => e.Category).ToList(),
+                _ => eventsList.OrderBy(e => e.Date).ToList()
+            };
+
             ViewBag.Categories = categories.OrderBy(c => c).ToList();
             ViewBag.RecentlyViewed = recentlyViewedEvents.ToList();
-         
+            ViewBag.CurrentSearch = search;
+            ViewBag.CurrentCategory = categoryFilter;
+            ViewBag.CurrentSort = sortOrder;
+
             return View(eventsList);
         }
-
 
         [HttpPost]
         public IActionResult ViewEvent(string eventName)
@@ -80,18 +85,11 @@ namespace Municipal_Services_Portal.Controllers
 
             if (evt != null)
             {
-                recentlyViewedEvents = new Stack<Event>(
-                    recentlyViewedEvents.Where(e => e.eventName != eventName)
-                );
-
+                recentlyViewedEvents = new Stack<Event>(recentlyViewedEvents.Where(e => e.eventName != eventName));
                 recentlyViewedEvents.Push(evt);
 
                 if (recentlyViewedEvents.Count > 5)
-                {
-                    recentlyViewedEvents = new Stack<Event>(
-                        recentlyViewedEvents.Take(5).Reverse()
-                    );
-                }
+                    recentlyViewedEvents = new Stack<Event>(recentlyViewedEvents.Take(5).Reverse());
             }
             return RedirectToAction("Events");
         }
